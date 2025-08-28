1557ee9024ed44393db81e2d12b164409a209eca
Diego <diego.bolanosvillalobos@ucr.ac.cr>
2025-08-28 09:30:02 -0600

    Lab 3 completado

---
+++ Laboratorio3_C21256/mi primer proyecto/frontend-lab/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <CountriesList/>
+</template>
+
+<script>
+import CountriesList from './components/CountriesList.vue';
+export default {
+  name: 'App',
+  components: {
+    CountriesList,
+  }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
